67fe39b4952e2b15522848d72ab62c10bbb3a4b4
NicoMaker <nicola.marano02gmail,com>
2025-03-17 10:07:44 +0100

    refactor contact form to use mailto link and remove server-side email handling

--- Code/App.js
+++ Code/App.js
@@ -1,41 +1,33 @@
-document
-  .getElementById("contactForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+function sendEmail(event) {
+  event.preventDefault(); // Preveniamo il comportamento predefinito del form (invio)
 
-    // Preleva i valori dal modulo
-    const name = document.getElementById("name").value,
-      email = document.getElementById("email").value,
-      subject = document.getElementById("oggetto").value,
-      message = document.getElementById("message").value;
+  // Recupera i valori dal modulo
+  const name = getInputValue("name"),
+    email = getInputValue("email"),
+    oggetto = getInputValue("oggetto"),
+    message = getInputValue("message");
 
-    // Creazione dell'oggetto con i dati del form
-    const formData = {
-      name: name,
-      email: email,
-      subject: subject,
-      message: message,
-    };
+  // Crea il corpo del messaggio
+  const subject = createSubject(oggetto),
+    body = createBody(name, email, message);
 
-    // Richiesta al server per inviare l'email
-    fetch("/send-email", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.success) {
-          alert("Email inviata con successo!");
-          document.getElementById("contactForm").reset();
-        } else {
-          alert("Errore nell'invio dell'email!");
-        }
-      })
-      .catch((error) => {
-        console.error("Errore:", error);
-        alert("Errore nel contatto con il server!");
-      });
-  });
+  // Crea il link mailto
+  const mailtoLink = createMailtoLink(subject, body);
+
+  // Reindirizza l'utente al link mailto
+  window.location.href = mailtoLink;
+}
+
+// Funzione per ottenere il valore di un input
+const getInputValue = (inputId) => document.getElementById(inputId).value;
+
+// Funzione per creare l'oggetto dell'email
+const createSubject = (oggetto) => encodeURIComponent(oggetto);
+
+// Funzione per creare il corpo dell'email
+const createBody = (name, email, message) =>
+  encodeURIComponent(`Nome: ${name}\nEmail: ${email}\nMessaggio: ${message}`);
+
+// Funzione per creare il link mailto
+const createMailtoLink = (subject, body) =>
+  `mailto:nicola.marano02@gmail.com?subject=${subject}&body=${body}`;
