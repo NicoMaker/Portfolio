1e1ec188715e6691d44b7cc5fe5904507d9bb666
NicoMaker <nicola.marano02@gmail.com>
2025-03-19 14:54:30 +0100

    feat: implement smooth scrolling functionality for curriculum section with new arrow script

--- Code/JS/Arrow.js
+++ Code/JS/Arrow.js
@@ -1,4 +1,4 @@
-document.querySelector(".scroll-arrow").addEventListener("click", function (e) {
+document.querySelector(".scroll-arrow").addEventListener("click", (e) => {
   e.preventDefault();
   document.querySelector("#curriculum").scrollIntoView({
     behavior: "smooth",
