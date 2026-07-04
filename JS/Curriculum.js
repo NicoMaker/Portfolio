058839cf001f9fb3f260bb203d8e14028f161d4d
NicoMaker <nicola.marano02@gmail.com>
2026-07-04 15:53:29 +0200

    fix: Remove unnecessary line breaks in Curriculum sections for cleaner layout

--- JS/Curriculum.js
+++ JS/Curriculum.js
@@ -571,8 +571,6 @@ function renderAttestati(attestati) {
       <input type="search" class="search-input" placeholder="Cerca attestato (titolo, ente, descrizione...)">
       <button class="search-reset-btn" type="button"><i class='bx bx-x'></i></button>
     </div>
-    <br>
-    <br>
   `;
   sectionWrapper.appendChild(searchBar);
 
